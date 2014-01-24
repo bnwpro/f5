4e76b9457f2d21ea4ae8380e521afbfed1e07c87
Brendon McDonnell <brendon@nwpro.org>
2014-01-24 14:01:46 -0500

    1/24/14_a
    Responsive tweaks, fade footer on scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$(document).ready(function() {
+	$(window).scroll(function() {
+		$('.shadow-footer').fadeOut(2500);
+	})
+})
